6aece75599fb311955601b810e4866744a719056
LoLo <luolc.witty@gmail.com>
2016-02-28 19:21:23 +0800

    add statistics get method

--- tips-collection/js/index.js
+++ tips-collection/js/index.js
@@ -31,8 +31,36 @@ function sendContact(contact) {
 	});
 }
 
+function setStatistics() {
+	STATISTICS_URL = 'http://123.56.189.217/thecourses/service/web/getStatistics.php';
+	$.ajax({
+		type: "GET",
+		data: {
+			dataType: "jsonp"
+		},
+		url: STATISTICS_URL,
+		dataType: "jsonp",
+		jsonpCallback: "getStatisticsCallback",
+		success: function(response) {
+			// alert(response.code);
+			if (response.code == "1000") {
+				$("#text-joined-count").text(response.data['joined']);
+				$("#text-tips-count").text(response.data['tips']);
+				$("#text-views-count").text(response.data['views']);
+				return;
+			}
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+	       alert(XMLHttpRequest.status);
+	       alert(XMLHttpRequest.readyState);
+	       alert(textStatus);
+     	}
+	});
+}
+
 window.onload = function() {
 	$(document).ready(function() {
+		setStatistics();
 		$("#button-join-us").click(function() {
 			if ($.cookie("token") && $.cookie("name")) {
 				location.href = "./form.html";
